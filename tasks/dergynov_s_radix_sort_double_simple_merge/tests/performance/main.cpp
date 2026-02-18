06347f55dc6d358f176c5af28be1352d86034bf4
Sergey <sergeydergynov038.uniads@gmail.com>
2026-02-18 23:42:26 +0300

    Fix clang-tidy warnings in performance tests

--- tasks/dergynov_s_radix_sort_double_simple_merge/tests/performance/main.cpp
+++ tasks/dergynov_s_radix_sort_double_simple_merge/tests/performance/main.cpp
@@ -12,28 +12,22 @@
 namespace dergynov_s_radix_sort_double_simple_merge {
 namespace {
 
-class DergynovRadixSortPerfTests : public ::testing::Test {
- protected:
-  void SetUp() override {
-    const size_t kDataSize = 1000000;
-    data_.resize(kDataSize);
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<double> dist(-1000.0, 1000.0);
-    for (size_t i = 0; i < kDataSize; ++i) {
-      data_[i] = dist(gen);
-    }
-  }
-
-  std::vector<double> data_;
-};
+template <typename T>
+void ValidateTask(T &task) {
+  ASSERT_TRUE(task.Validation());
+}
 
 template <typename T>
-void ValidateAndProcess(T &task) {
-  ASSERT_TRUE(task.Validation());
+void ProcessTask(T &task) {
   ASSERT_TRUE(task.PreProcessing());
   ASSERT_TRUE(task.Run());
   ASSERT_TRUE(task.PostProcessing());
+}
+
+template <typename T>
+void ValidateAndProcess(T &task) {
+  ValidateTask(task);
+  ProcessTask(task);
 }
 
 template <typename T>
@@ -43,21 +37,37 @@ void CheckSortedAndSize(T &task, const std::vector<double> &data) {
   EXPECT_EQ(sorted.size(), data.size());
 }
 
+class DergynovRadixSortPerfTests : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    const size_t k_data_size = 1000000;
+    data.resize(k_data_size);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> dist(-1000.0, 1000.0);
+    for (size_t i = 0; i < k_data_size; ++i) {
+      data[i] = dist(gen);
+    }
+  }
+
+  std::vector<double> data;
+};
+
 TEST_F(DergynovRadixSortPerfTests, SeqPerformance) {
-  DergynovSRadixSortDoubleSimpleMergeSEQ task(data_);
+  DergynovSRadixSortDoubleSimpleMergeSEQ task(data);
   ValidateAndProcess(task);
-  CheckSortedAndSize(task, data_);
+  CheckSortedAndSize(task, data);
 }
 
 TEST_F(DergynovRadixSortPerfTests, MpiPerformance) {
   int rank = 0;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  DergynovSRadixSortDoubleSimpleMergeMPI task(data_);
+  DergynovSRadixSortDoubleSimpleMergeMPI task(data);
   ValidateAndProcess(task);
 
   if (rank == 0) {
-    CheckSortedAndSize(task, data_);
+    CheckSortedAndSize(task, data);
   }
 }
 
