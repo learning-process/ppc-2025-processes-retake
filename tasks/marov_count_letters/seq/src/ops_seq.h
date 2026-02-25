7ce6266369ea8f81702e4d711d299652605fa19b
Marofal <_email@marov2021@vk.com>
2026-02-25 14:29:49 +0000

    feat: add task1 variant 22 for marov

---
+++ tasks/marov_count_letters/seq/src/ops_seq.h
@@ -0,0 +1,8 @@
+#ifndef OPS_SEQ_H
+#define OPS_SEQ_H
+
+#include <string>
+
+int count_letters(const std::string& str);
+
+#endif
---
+++ tasks/marov_count_letters/tests/functional/ops_seq_tests.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include "ops_seq.h"
+
+TEST(count_letters_seq, basic) {
+    std::string str = "Hello, world!";
+    EXPECT_EQ(count_letters(str), 10);
+}
+
+TEST(count_letters_seq, no_letters) {
+    std::string str = "12345!@#$%";
+    EXPECT_EQ(count_letters(str), 0);
+}
+
+TEST(count_letters_seq, empty) {
+    std::string str = "";
+    EXPECT_EQ(count_letters(str), 0);
+}
