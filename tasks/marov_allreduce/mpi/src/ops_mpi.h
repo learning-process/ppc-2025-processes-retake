340ad5de1b7925359f3b84a1f20c2d8ed42b98d5
Marofal <_email@marov2021@vk.com>
2026-02-25 14:51:51 +0000

    feat: add task2 (allreduce) and task3 (radix sort) for marov

---
+++ tasks/marov_allreduce/mpi/src/ops_mpi.h
@@ -0,0 +1,9 @@
+#ifndef OPS_MPI_H
+#define OPS_MPI_H
+
+#include <mpi.h>
+
+int my_allreduce(const void *sendbuf, void *recvbuf, int count,
+                 MPI_Datatype datatype, MPI_Op op, MPI_Comm comm);
+
+#endif
---
+++ tasks/marov_radix_sort_double/tests/functional/ops_seq_tests.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+#include "ops_seq.h"
+#include <algorithm>
+#include <vector>
+
+TEST(radix_sort_double_test, basic_sort) {
+    std::vector<double> arr = {3.14, 1.41, 2.71, 0.58, 1.61};
+    std::vector<double> expected = arr;
+    std::sort(expected.begin(), expected.end());
+    
+    radix_sort_double(arr);
+    
+    EXPECT_EQ(arr, expected);
+}
+
+TEST(radix_sort_double_test, negative_numbers) {
+    std::vector<double> arr = {-2.5, 3.7, -1.1, 0.0, 4.2, -3.3};
+    std::vector<double> expected = arr;
+    std::sort(expected.begin(), expected.end());
+    
+    radix_sort_double(arr);
+    
+    EXPECT_EQ(arr, expected);
+}
