f38d2c53b921e2f1be7373a897ebc64a06408b3b
Notexxx <artemkrasavin1337@gmail.com>
2026-02-25 20:43:22 +0300

    new push

--- tasks/krasavin_a_image_smoothing/seq/include/ops_seq.hpp
+++ tasks/krasavin_a_image_smoothing/seq/include/ops_seq.hpp
@@ -5,6 +5,7 @@
 #include "krasavin_a_image_smoothing/common/include/common.hpp"
 #include "task/include/task.hpp"
 
+
 namespace krasavin_a_image_smoothing {
 
 class KrasavinAImageSmoothingSEQ : public BaseTask {
