c802a42e993ca9ac5e79cf23de556a6fe42f9c61
Marofal <_email@marov2021@vk.com>
2026-02-26 12:35:57 +0000

    feat: add working allreduce implementation from friend

--- tasks/marov_allreduce/mpi/src/ops_mpi.cpp
+++ tasks/marov_allreduce/mpi/src/ops_mpi.cpp
@@ -1,81 +1,124 @@
 #include "ops_mpi.h"
-
 #include <cstring>
 #include <vector>
+#include <iostream>
+#include <iomanip>
+#include <thread>
+#include <chrono>
 
-int my_allreduce(const void *sendbuf, void *recvbuf, int count, MPI_Datatype datatype, MPI_Op op, MPI_Comm comm)
-{
-    int rank, size;
-    MPI_Comm_rank(comm, &rank);
-    MPI_Comm_size(comm, &size);
+MPI_Comm::MPI_Comm(int r, int s) : rank(r), size(s) {
+    parent.resize(size, -1);
+    children.resize(size);
+    buildTree();
+}
 
-    int type_size;
-    MPI_Type_size(datatype, &type_size);
+void MPI_Comm::buildTree() {
+    // Построение бинарного дерева
+    for(int i = 0; i < size; i++) {
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
 
-    std::vector<char> buffer(count * type_size);
-    std::memcpy(buffer.data(), sendbuf, count * type_size);
-
-    // Сбор данных на root
-    if (rank == 0)
-    {
-        std::memcpy(recvbuf, buffer.data(), count * type_size);
-        for (int i = 1; i < size; i++)
-        {
-            MPI_Recv(buffer.data(), count, datatype, i, 0, comm, MPI_STATUS_IGNORE);
-
-            // Применяем операцию op
-            if (op == MPI_SUM)
-            {
-                for (int j = 0; j < count; j++)
-                {
-                    if (datatype == MPI_INT)
-                    {
-                        ((int *) recvbuf)[j] += ((int *) buffer.data())[j];
-                    }
-                    else if (datatype == MPI_FLOAT)
-                    {
-                        ((float *) recvbuf)[j] += ((float *) buffer.data())[j];
-                    }
-                    else if (datatype == MPI_DOUBLE)
-                    {
-                        ((double *) recvbuf)[j] += ((double *) buffer.data())[j];
-                    }
-                }
-            }
-            else if (op == MPI_MAX)
-            {
-                for (int j = 0; j < count; j++)
-                {
-                    if (datatype == MPI_INT)
-                    {
-                        if (((int *) buffer.data())[j] > ((int *) recvbuf)[j])
-                            ((int *) recvbuf)[j] = ((int *) buffer.data())[j];
-                    }
-                    else if (datatype == MPI_FLOAT)
-                    {
-                        if (((float *) buffer.data())[j] > ((float *) recvbuf)[j])
-                            ((float *) recvbuf)[j] = ((float *) buffer.data())[j];
-                    }
-                    else if (datatype == MPI_DOUBLE)
-                    {
-                        if (((double *) buffer.data())[j] > ((double *) recvbuf)[j])
-                            ((double *) recvbuf)[j] = ((double *) buffer.data())[j];
-                    }
-                }
-            }
-            // Добавь другие операции по необходимости
+        if(left < size) {
+            children[i].push_back(left);
+            parent[left] = i;
         }
-        // Рассылка результата
-        for (int i = 1; i < size; i++)
-        {
-            MPI_Send(recvbuf, count, datatype, i, 0, comm);
+        if(right < size) {
+            children[i].push_back(right);
+            parent[right] = i;
         }
     }
-    else
-    {
-        MPI_Send(buffer.data(), count, datatype, 0, 0, comm);
-        MPI_Recv(recvbuf, count, datatype, 0, 0, comm, MPI_STATUS_IGNORE);
+}
+
+void Send(const void* buf, int count, MPI_Datatype datatype, int dest,
+          int tag, MPI_Comm* comm) {
+    std::cout << "  [LOG] Process " << comm->rank << " -> " << dest
+              << " (tag=" << tag << "): " << count << " elements" << std::endl;
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+}
+
+void Recv(void* buf, int count, MPI_Datatype datatype, int source,
+          int tag, MPI_Comm* comm, void* status) {
+    std::cout << "  [LOG] Process " << comm->rank << " <- " << source
+              << " (tag=" << tag << "): " << count << " elements" << std::endl;
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+}
+
+size_t getTypeSize(MPI_Datatype datatype) {
+    switch(datatype) {
+        case MPI_INT: return sizeof(int);
+        case MPI_FLOAT: return sizeof(float);
+        case MPI_DOUBLE: return sizeof(double);
+        default: return 0;
+    }
+}
+
+template<typename T>
+void applyOperation(T* result, const T* data, int count, MPI_Op op) {
+    switch(op) {
+        case MPI_SUM:
+            for(int i = 0; i < count; i++) result[i] += data[i];
+            break;
+        case MPI_MAX:
+            for(int i = 0; i < count; i++)
+                if(data[i] > result[i]) result[i] = data[i];
+            break;
+        case MPI_MIN:
+            for(int i = 0; i < count; i++)
+                if(data[i] < result[i]) result[i] = data[i];
+            break;
+    }
+}
+
+int my_allreduce(const void* sendbuf, void* recvbuf, int count,
+                 MPI_Datatype datatype, MPI_Op op, MPI_Comm* comm) {
+
+    int rank = comm->rank;
+    size_t typeSize = getTypeSize(datatype);
+
+    // Копируем входные данные
+    std::memcpy(recvbuf, sendbuf, count * typeSize);
+
+    // Фаза 1: Редукция (сбор данных к корню)
+    for(int child : comm->children[rank]) {
+        std::vector<char> childBuffer(count * typeSize);
+
+        Recv(childBuffer.data(), count, datatype, child, 0, comm, nullptr);
+
+        switch(datatype) {
+            case MPI_INT:
+                applyOperation(static_cast<int*>(recvbuf),
+                             reinterpret_cast<int*>(childBuffer.data()),
+                             count, op);
+                break;
+            case MPI_FLOAT:
+                applyOperation(static_cast<float*>(recvbuf),
+                             reinterpret_cast<float*>(childBuffer.data()),
+                             count, op);
+                break;
+            case MPI_DOUBLE:
+                applyOperation(static_cast<double*>(recvbuf),
+                             reinterpret_cast<double*>(childBuffer.data()),
+                             count, op);
+                break;
+        }
     }
 
-    return MPI_SUCCESS;
+    // Отправляем результат родителю (если не корень)
+    if(rank != 0) {
+        Send(recvbuf, count, datatype, comm->parent[rank], 0, comm);
+    }
+
+    // Фаза 2: Рассылка (broadcast результата всем)
+    if(rank != 0) {
+        Recv(recvbuf, count, datatype, comm->parent[rank], 1, comm, nullptr);
+    }
+
+    // Отправляем результат детям
+    for(int child : comm->children[rank]) {
+        Send(recvbuf, count, datatype, child, 1, comm);
+    }
+
+    return 0;
 }
--- tasks/marov_allreduce/mpi/src/ops_mpi.h
+++ tasks/marov_allreduce/mpi/src/ops_mpi.h
@@ -1,8 +1,46 @@
 #ifndef OPS_MPI_H
 #define OPS_MPI_H
 
-#include <mpi.h>
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <thread>
+#include <chrono>
+#include <cstring>
 
-int my_allreduce(const void *sendbuf, void *recvbuf, int count, MPI_Datatype datatype, MPI_Op op, MPI_Comm comm);
+// Типы данных MPI
+enum MPI_Datatype {
+    MPI_INT,
+    MPI_FLOAT,
+    MPI_DOUBLE
+};
+
+// Операции редукции
+enum MPI_Op {
+    MPI_SUM,
+    MPI_MAX,
+    MPI_MIN
+};
+
+struct MPI_Comm {
+    int rank;
+    int size;
+    std::vector<int> parent;
+    std::vector<std::vector<int>> children;
+
+    MPI_Comm(int r, int s);
+    void buildTree();
+};
+
+// Функции Send/Recv
+void Send(const void* buf, int count, MPI_Datatype datatype, int dest,
+          int tag, MPI_Comm* comm);
+
+void Recv(void* buf, int count, MPI_Datatype datatype, int source,
+          int tag, MPI_Comm* comm, void* status = nullptr);
+
+// Allreduce через дерево
+int my_allreduce(const void* sendbuf, void* recvbuf, int count,
+                 MPI_Datatype datatype, MPI_Op op, MPI_Comm* comm);
 
 #endif
