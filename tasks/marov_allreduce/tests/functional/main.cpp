1736b0bc2c1d066c0980d0773f792badaf914343
marofal <marov2021@vk.com>
2026-02-27 20:06:30 +0300

    marov: fix clang-tidy warnings

--- tasks/marov_allreduce/tests/functional/main.cpp
+++ tasks/marov_allreduce/tests/functional/main.cpp
@@ -1,19 +1,21 @@
 #include <gtest/gtest.h>
+#include <array>
+#include <string>
 #include <vector>
 
 #include "marov_allreduce/mpi/include/ops_mpi.h"
 
 TEST(marovAllreduceMpi, sumInts) {
-  const int kNumProcs = 7;
-  std::vector<int> send_values(kNumProcs);
-  std::vector<int> recv_values(kNumProcs);
+  const int k_num_procs = 7;
+  std::vector<int> send_values(k_num_procs);
+  std::vector<int> recv_values(k_num_procs);
 
-  for (int i = 0; i < kNumProcs; i++) {
+  for (int i = 0; i < k_num_procs; i++) {
     send_values[i] = i + 1;
   }
 
-  for (int rank = 0; rank < kNumProcs; rank++) {
-    MPI_Comm comm(rank, kNumProcs);
+  for (int rank = 0; rank < k_num_procs; rank++) {
+    MPI_Comm comm(rank, k_num_procs);
     int send_data = send_values[rank];
     int recv_data = 0;
 
@@ -31,16 +33,16 @@ TEST(marovAllreduceMpi, sumInts) {
 }
 
 TEST(marovAllreduceMpi, maxFloats) {
-  const int kNumProcs = 7;
-  std::vector<float> send_values(kNumProcs);
-  std::vector<float> recv_values(kNumProcs);
+  const int k_num_procs = 7;
+  std::vector<float> send_values(k_num_procs);
+  std::vector<float> recv_values(k_num_procs);
 
-  for (int i = 0; i < kNumProcs; i++) {
+  for (int i = 0; i < k_num_procs; i++) {
     send_values[i] = static_cast<float>(i) * 1.5F;
   }
 
-  for (int rank = 0; rank < kNumProcs; rank++) {
-    MPI_Comm comm(rank, kNumProcs);
+  for (int rank = 0; rank < k_num_procs; rank++) {
+    MPI_Comm comm(rank, k_num_procs);
     float send_data = send_values[rank];
     float recv_data = 0;
 
@@ -50,9 +52,7 @@ TEST(marovAllreduceMpi, maxFloats) {
 
   float expected = 0;
   for (float v : send_values) {
-    if (v > expected) {
-      expected = v;
-    }
+    expected = std::max(v, expected);
   }
   for (float r : recv_values) {
     EXPECT_EQ(r, expected);
@@ -60,47 +60,46 @@ TEST(marovAllreduceMpi, maxFloats) {
 }
 
 TEST(marovAllreduceMpi, minDoubles) {
-  const int kNumProcs = 7;
-  const int kArraySize = 3;
+  const int k_num_procs = 7;
+  const int k_array_size = 3;
   std::vector<std::vector<double>> send_values(
-      kNumProcs, std::vector<double>(kArraySize));
+      k_num_procs, std::vector<double>(k_array_size));
   std::vector<std::vector<double>> recv_values(
-      kNumProcs, std::vector<double>(kArraySize));
+      k_num_procs, std::vector<double>(k_array_size));
 
-  for (int i = 0; i < kNumProcs; i++) {
+  for (int i = 0; i < k_num_procs; i++) {
     send_values[i][0] = i + 1.0;
     send_values[i][1] = i * 2.0;
     send_values[i][2] = i + 0.5;
   }
 
-  for (int rank = 0; rank < kNumProcs; rank++) {
-    MPI_Comm comm(rank, kNumProcs);
-    double send_data[kArraySize];
-    double recv_data[kArraySize];
+  for (int rank = 0; rank < k_num_procs; rank++) {
+    MPI_Comm comm(rank, k_num_procs);
+    std::array<double, k_array_size> send_data{};
+    std::array<double, k_array_size> recv_data{};
 
-    for (int j = 0; j < kArraySize; j++) {
+    for (int j = 0; j < k_array_size; j++) {
       send_data[j] = send_values[rank][j];
       recv_data[j] = 0;
     }
 
-    my_allreduce(send_data, recv_data, kArraySize, MPI_DOUBLE, MPI_MIN, &comm);
+    my_allreduce(send_data.data(), recv_data.data(), k_array_size, MPI_DOUBLE,
+                 MPI_MIN, &comm);
 
-    for (int j = 0; j < kArraySize; j++) {
+    for (int j = 0; j < k_array_size; j++) {
       recv_values[rank][j] = recv_data[j];
     }
   }
 
-  std::vector<double> expected(kArraySize, 1e9);
-  for (int i = 0; i < kNumProcs; i++) {
-    for (int j = 0; j < kArraySize; j++) {
-      if (send_values[i][j] < expected[j]) {
-        expected[j] = send_values[i][j];
-      }
+  std::vector<double> expected(k_array_size, 1e9);
+  for (int i = 0; i < k_num_procs; i++) {
+    for (int j = 0; j < k_array_size; j++) {
+      expected[j] = std::min(send_values[i][j], expected[j]);
     }
   }
 
-  for (int rank = 0; rank < kNumProcs; rank++) {
-    for (int j = 0; j < kArraySize; j++) {
+  for (int rank = 0; rank < k_num_procs; rank++) {
+    for (int j = 0; j < k_array_size; j++) {
       EXPECT_EQ(recv_values[rank][j], expected[j]);
     }
   }
--- tasks/marov_radix_sort_double/tests/functional/main.cpp
+++ tasks/marov_radix_sort_double/tests/functional/main.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 
+#include <string>
 #include <tuple>
 #include <vector>
 
