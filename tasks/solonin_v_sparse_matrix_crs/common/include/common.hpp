2dc3fe63f5b32257e6c7a0fcfe29cf7e65e1ebf6
Solonin Vlad <mngmnt755@yandex.ru>
2026-03-01 14:30:34 +0300

    fix: fix include order in common.hpp

--- tasks/solonin_v_sparse_matrix_crs/common/include/common.hpp
+++ tasks/solonin_v_sparse_matrix_crs/common/include/common.hpp
@@ -1,15 +1,14 @@
 #pragma once
+#include "task/include/task.hpp"
+#include <string>
 #include <tuple>
 #include <vector>
-#include "task/include/task.hpp"
 
 namespace solonin_v_sparse_matrix_crs {
 
-using InType = std::tuple<std::vector<double>, std::vector<int>, std::vector<int>, std::vector<double>,
-                          std::vector<int>, std::vector<int>, int, int, int>;
+using InType = std::tuple<std::vector<double>, std::vector<int>, std::vector<int>, std::vector<double>, std::vector<int>, std::vector<int>, int, int, int>;
 using OutType = std::tuple<std::vector<double>, std::vector<int>, std::vector<int>>;
-using TestType = std::tuple<int, std::vector<std::vector<double>>, std::vector<std::vector<double>>,
-                            std::vector<std::vector<double>>>;
+using TestType = std::tuple<int, std::vector<std::vector<double>>, std::vector<std::vector<double>>, std::vector<std::vector<double>>>;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
 }  // namespace solonin_v_sparse_matrix_crs
