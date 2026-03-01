8cdd7a9f055e8e6dde6944762d014438ca4342cf
Solonin Vlad <mngmnt755@yandex.ru>
2026-03-01 14:51:59 +0300

    fix: rewrite all files in Nalitov style to pass clang-format

--- tasks/solonin_v_col_min_matrix/common/include/common.hpp
+++ tasks/solonin_v_col_min_matrix/common/include/common.hpp
@@ -1,16 +1,1 @@
-#pragma once
-
-#include "task/include/task.hpp"
-
-#include <string>
-#include <tuple>
-#include <vector>
-
-namespace solonin_v_col_min_matrix {
-
-using InType = int;
-using OutType = std::vector<int>;
-using TestType = std::tuple<int, std::string>;
-using BaseTask = ppc::task::Task<InType, OutType>;
-
-}  // namespace solonin_v_col_min_matrix
+404: Not Found
