c242a5abe2f3b8dea0ef649b1587d8e208cd5538
Solonin Vlad <mngmnt755@yandex.ru>
2026-03-01 14:29:52 +0300

    fix: fix include order in common.hpp

--- tasks/solonin_v_scatter/common/include/common.hpp
+++ tasks/solonin_v_scatter/common/include/common.hpp
@@ -1,15 +1,14 @@
 #pragma once
+#include "task/include/task.hpp"
+#include <string>
 #include <tuple>
 #include <vector>
-#include "task/include/task.hpp"
 
 namespace solonin_v_scatter {
 
-// InType: (send_buf, send_count, root)
-// OutType: recv_buf (each process receives send_count elements)
 using InType = std::tuple<std::vector<int>, int, int>;
 using OutType = std::vector<int>;
-using TestType = std::tuple<int, std::vector<int>, int, int>;  // (id, data, send_count, root)
+using TestType = std::tuple<int, std::vector<int>, int, int>;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
 }  // namespace solonin_v_scatter
