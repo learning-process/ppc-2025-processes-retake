eb53f124e182939b9da58dadba8efbc07e2bab0b
Solonin Vlad <mngmnt755@yandex.ru>
2026-03-01 14:26:31 +0300

    fix: fix include order in common.hpp

--- tasks/solonin_v_col_min_matrix/common/include/common.hpp
+++ tasks/solonin_v_col_min_matrix/common/include/common.hpp
@@ -1,12 +1,14 @@
 #pragma once
+#include "task/include/task.hpp"
 #include <string>
 #include <tuple>
 #include <vector>
-#include "task/include/task.hpp"
 
 namespace solonin_v_col_min_matrix {
+
 using InType = int;
 using OutType = std::vector<int>;
 using TestType = std::tuple<int, std::string>;
 using BaseTask = ppc::task::Task<InType, OutType>;
+
 }  // namespace solonin_v_col_min_matrix
