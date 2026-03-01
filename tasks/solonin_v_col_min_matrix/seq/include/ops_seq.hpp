3dde712a512162b1c0bd27f301c6cfc6a97143c6
Solonin Vlad <mngmnt755@yandex.ru>
2026-03-01 14:53:08 +0300

    Revert "fix: rewrite all files in Nalitov style to pass clang-format"
    This reverts commit 8cdd7a9f055e8e6dde6944762d014438ca4342cf.

--- tasks/solonin_v_col_min_matrix/seq/include/ops_seq.hpp
+++ tasks/solonin_v_col_min_matrix/seq/include/ops_seq.hpp
@@ -1,1 +1,17 @@
-404: Not Found
+#pragma once
+#include "solonin_v_col_min_matrix/common/include/common.hpp"
+#include "task/include/task.hpp"
+
+namespace solonin_v_col_min_matrix {
+class SoloninVMinMatrixSEQ : public BaseTask {
+ public:
+  static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() { return ppc::task::TypeOfTask::kSEQ; }
+  explicit SoloninVMinMatrixSEQ(const InType &in);
+
+ private:
+  bool ValidationImpl() override;
+  bool PreProcessingImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+};
+}  // namespace solonin_v_col_min_matrix
